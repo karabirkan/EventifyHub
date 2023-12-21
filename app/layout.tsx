e7d36bc751756fc9781ebfa3dae1d0f9f058a730
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2023-12-21 18:29:33 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Eventify Hub",
+  description: "Create and manage your events by Eventify Hub ",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+    </html>
+  );
+}

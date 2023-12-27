0fbb699f98e8d04c9c60e6568b0895bd604ff1d3
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2023-12-27 15:22:37 +0300

    feat: get ticket button-function

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
 import {
   getEventById,
@@ -51,6 +52,7 @@ const EventDetails = async ({
               </div>
             </div>
             {/* {checkout button} */}
+            <CheckoutButton event={event} />
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
                 <Image
---
+++ components/shared/CheckoutButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { IEvent } from "@/lib/database/models/event.model";
+import { SignedOut } from "@clerk/clerk-react";
+import { SignedIn, useUser } from "@clerk/nextjs";
+import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
+const CheckoutButton = ({ event }: { event: IEvent }) => {
+  const { user } = useUser();
+  const userId = user?.publicMetadata.userId as string;
+  const hasEventFinished = new Date(event.endDateTime) < new Date();
+
+  return (
+    <div className="flex items-center gap-3">
+      {hasEventFinished ? (
+        <p className="p-2 text-red-400">
+          Sorry, tickets are no longer avaiable
+        </p>
+      ) : (
+        <>
+          <SignedOut>
+            <Button asChild className="button rounded-full" size="lg">
+              <Link href="/sign-in">Get Tickets</Link>
+            </Button>
+          </SignedOut>
+          <SignedIn></SignedIn>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CheckoutButton;

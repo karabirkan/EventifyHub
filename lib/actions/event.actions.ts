8d749687744c8d3c0feff06e2598cc434f287f0b
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2023-12-27 11:21:23 +0300

    feat: get all events actions

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -4,7 +4,7 @@ import Category from "../database/models/category.model";
 import Event from "../database/models/event.model";
 import User from "../database/models/user.model";
 import { handleError } from "../utils";
-import { CreateEventParams } from "./../../types/index";
+import { CreateEventParams, GetAllEventsParams } from "./../../types/index";
 
 const populateEvent = async (query: any) => {
   return query
@@ -45,6 +45,34 @@ export const getEventById = async (eventId: string) => {
     if (!eventId) throw new Error("Event not found");
 
     return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getAllEvents = async ({
+  query,
+  limit = 6,
+  page,
+  category,
+}: GetAllEventsParams) => {
+  try {
+    await connectToDatabase();
+
+    const conditions = {};
+
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(0)
+      .limit(limit);
+
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
+
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    };
   } catch (error) {
     handleError(error);
   }
